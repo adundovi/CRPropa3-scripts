9387f578ba41eeb5cfe56990c16643359e7225ed
Andrej Dundovic <andrej.dundovic@desy.de>
2016-11-12 20:11:31 +0100

    muparser install

--- deploy_crpropa/install_deps.sh
+++ deploy_crpropa/install_deps.sh
@@ -24,7 +24,6 @@ function pip_install {
 }
 
 # SWIG install
-
 SWIG_FILE="swig-3.0.7.tar.gz"
 SWIG_URL="http://prdownloads.sourceforge.net/swig/$SWIG_FILE"
 SWIG_BUILD=$CRPROPA_DIR"/swig_build"
@@ -33,7 +32,6 @@ SWIG_OPTIONS=""
 download_install $SWIG_FILE $SWIG_URL $SWIG_BUILD $SWIG_OPTIONS
 
 # FFTW3 install
-
 FFTW_FILE="fftw-3.3.4.tar.gz"
 FFTW_URL="http://www.fftw.org/$FFTW_FILE"
 FFTW_BUILD=$CRPROPA_DIR"/fftw_build"
@@ -41,4 +39,13 @@ FFTW_OPTIONS="--enable-float --enable-shared --with-pic"
 
 download_install $FFTW_FILE $FFTW_URL $FFTW_BUILD $FFTW_OPTIONS
 
+# muparser install
+MUPARS_FILE="v2.2.5.tar.gz"
+MUPARS_URL="https://github.com/beltoforion/muparser/archive/$MUPARS_FILE"
+MUPARS_BUILD=$CRPROPA_DIR"/muparser_build"
+MUPARS_OPTIONS=""
+
+download_install $MUPARS_FILE $MUPARS_URL $MUPARS_BUILD $MUPARS_OPTIONS
+
+# numpy
 pip_install numpy
